9618862d725fdb1472fb5450df402a0734b07905
Pramodh <pormahd@gmail.com>
2021-06-19 22:17:26 +1000

    Selects random file from sound dir

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
+const fs = require('fs');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const timerMin = 300000
 const timerMax = 1800000
 
+const soundsDir = './sounds';
+
 const guild = ""
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
+  let sounds = fs.readdirSync(soundsDir);
   let channels = [];
+
+  // get sound files
+
+  console.log("\nAvailable sound files:");
+  // console.log(sounds);
+
+  sounds.forEach(sound => {
+    console.log(sound);
+  });
 
   // console.log(channels);
 
@@ -28,10 +42,10 @@ client.on('ready', () => {
 
   // console.log(channels);
 
-  pop(channels);
+  pop(channels, sounds);
 });
 
-function pop(channels) {
+function pop(channels, sounds) {
   console.log('');
   console.log((new Date()).toLocaleString());
 
@@ -56,10 +70,16 @@ function pop(channels) {
     let target = targetChannels[Math.floor(Math.random() * targetChannels.length)];
     console.log("Selected target channel: " + target.name);
 
+    // get random sound file from sounds
+    let sound = sounds[Math.floor(Math.random() * sounds.length)];
+    let soundFile = soundsDir + '/' + sound;
+
+    // soundFile = './sounds/pop-bass.opus';
+
     target.join()
       .then(connection => {
-        console.log("P O P");
-        let dispatcher = connection.play('./pop.wav', { volume: 0.75 });
+        console.log(`P O P (${sound})`);
+        let dispatcher = connection.play(soundFile, { volume: 0.75 });
 
         // leave voice channel after playing sound
         dispatcher.on('speaking', speaking => {
@@ -75,7 +95,7 @@ function pop(channels) {
   let sec = Math.floor((timeout / 1000) % 60);
 
   console.log(`\nNext pop in ${min} mins and ${sec} seconds`);
-  setTimeout(pop, timeout, channels);
+  setTimeout(pop, timeout, channels, sounds);
 
 }
 
